fb0f662115f893be0d03db53c4768e157e32839a
Chris Southwick <csouthwick@live.com>
2020-10-28 01:29:34 -0400

    add password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.get('/', (req, res) => {
-  User.findAll({ attributes: { exclude: ['password'] } })
+  User.findAll()
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -45,6 +45,7 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
