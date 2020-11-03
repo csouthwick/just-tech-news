399acb7a07f25f25d24e76690dc6422566bcd169
Chris Southwick <csouthwick@live.com>
2020-11-03 00:43:36 -0500

    add login, signup, and session features

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize })
+};
+
 const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const routes = require('./controllers');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
